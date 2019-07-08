6dd6924734b1fe1022500221318507e27bc9deae
mbaka <salliedailey@gmail.com>
2019-07-08 23:40:55 +0200

    [BUG] custom,

--- R/mmhc.R
+++ R/mmhc.R
@@ -237,7 +237,7 @@ mmpc <- function( data, node.sizes, cont.nodes = NULL, chi.th = 0.05,
     for( j in 1:n.layers )
       layer.mat[ layering==i, layering==j ] <- layer.struct[i,j]
   diag(layer.mat) <- 0
-  print(!is.null(custom))
+  print(layer.mat)
   if (!is.null(custom)){
     layer.mat<-custom
     print(layer.struct)
